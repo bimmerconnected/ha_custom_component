8098d4bfb342f2e0eb6b390c568bb0374741ae2d
Gerard <gerard33@users.noreply.github.com>
2020-03-31 20:08:46 +0200

    Update notify.py
    Remove services send_message and send_poi
    Add send_poi to notify

--- bmw_connected_drive/notify.py
+++ bmw_connected_drive/notify.py
@@ -2,13 +2,24 @@
 import logging
 
 from homeassistant.components.notify import (
+    ATTR_DATA,
     ATTR_TITLE,
     ATTR_TITLE_DEFAULT,
     BaseNotificationService,
 )
+from homeassistant.const import (
+    ATTR_LATITUDE,
+    ATTR_LOCATION,
+    ATTR_LONGITUDE,
+    ATTR_NAME,
+)
 
 from . import DOMAIN as BMW_DOMAIN
-from .const import SUBJECT, TEXT
+
+ATTR_LAT = "lat"
+ATTR_LON = "lon"
+ATTR_SUBJECT = "subject"
+ATTR_TEXT = "text"
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,11 +27,11 @@ _LOGGER = logging.getLogger(__name__)
 def get_service(hass, config, discovery_info=None):
     """Get the BMW notification service."""
     accounts = hass.data[BMW_DOMAIN]
-    _LOGGER.error("Found BMW accounts: %s", ", ".join([a.name for a in accounts]))
+    _LOGGER.error("Found BMW accounts: %s", ", ".join([a.name for a in accounts])) # TODO
     #TODO the above logger is set to error to see the message in the log easily -> change to debug
     for account in accounts:
         for vehicle in account.account.vehicles:
-            return BMWNotificationService(vehicle)
+            return BMWNotificationService(vehicle) # TODO
 
 
 class BMWNotificationService(BaseNotificationService):
@@ -29,13 +40,30 @@ class BMWNotificationService(BaseNotificationService):
     def __init__(self, vehicle):
         """Set up the notification service."""
         self._vehicle = vehicle
-        self.targets = {self._vehicle.name: self._vehicle}
+        self.targets = {self._vehicle.name: self._vehicle} # TODO
 
     def send_message(self, message="", **kwargs):
         """Send the message to the car."""
-        _LOGGER.error("Sending message to %s", self._vehicle.name)
+        _LOGGER.error("Sending message to %s", self._vehicle.name) # TODO
         #TODO the above logger is set to error to see the message in the log easily -> change to debug
 
         # Extract params from data dict
+        data = kwargs.get(ATTR_DATA)
         title = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
-        self._vehicle.remote_services.trigger_send_message({TEXT: message, SUBJECT: title})
+
+        # Check if message is a POI
+        if data is not None and ATTR_LOCATION in data:
+            self._vehicle.remote_services.trigger_send_poi(
+                {
+                    ATTR_LAT: data[ATTR_LOCATION].get(ATTR_LATITUDE),
+                    ATTR_LON: data[ATTR_LOCATION].get(ATTR_LONGITUDE),
+                    ATTR_NAME: data[ATTR_LOCATION].get(ATTR_NAME),
+                }
+            )
+        else:
+            self._vehicle.remote_services.trigger_send_message(
+                {
+                    ATTR_TEXT: message,
+                    ATTR_SUBJECT: title,
+                }
+            )
