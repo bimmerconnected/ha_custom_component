5e8dd32b7d3586dc1ffd0ca6684216b7c8496bbe
Gerard <gerard33@users.noreply.github.com>
2020-04-01 00:35:43 +0200

    Update notify.py
    * Removed account name in target
    * `postalCode` to `postal_code`
    * Changed `message=None` to `message=""` as this is the default in other notify components
    * Changed error logging from error to debug
    * Code formatted using black

--- bmw_connected_drive/notify.py
+++ bmw_connected_drive/notify.py
@@ -2,8 +2,8 @@
 import logging
 
 from homeassistant.components.notify import (
-    ATTR_TARGET,
     ATTR_DATA,
+    ATTR_TARGET,
     ATTR_TITLE,
     ATTR_TITLE_DEFAULT,
     BaseNotificationService,
@@ -18,8 +18,8 @@ from homeassistant.const import (
 from . import DOMAIN as BMW_DOMAIN
 
 ATTR_LAT = "lat"
+ATTR_LOCATION_ATTRIBUTES = ["street", "city", "postal_code", "country"]
 ATTR_LON = "lon"
-ATTR_LOCATION_ATTRIBUTES = ["street", "city", "postalCode", "country"]
 ATTR_SUBJECT = "subject"
 ATTR_TEXT = "text"
 
@@ -29,9 +29,8 @@ _LOGGER = logging.getLogger(__name__)
 def get_service(hass, config, discovery_info=None):
     """Get the BMW notification service."""
     accounts = hass.data[BMW_DOMAIN]
-    _LOGGER.error("Found BMW accounts: %s", ", ".join([a.name for a in accounts])) # TODO
-    #TODO the above logger is set to error to see the message in the log easily -> change to debug
-    return BMWNotificationService(accounts) 
+    _LOGGER.debug("Found BMW accounts: %s", ", ".join([a.name for a in accounts]))
+    return BMWNotificationService(accounts)
 
 
 class BMWNotificationService(BaseNotificationService):
@@ -39,17 +38,15 @@ class BMWNotificationService(BaseNotificationService):
 
     def __init__(self, accounts):
         """Set up the notification service."""
-        # self.targets = {v.name: v for v in account.account.vehicles}
         self.targets = {}
         for account in accounts:
-            self.targets.update({"{}_{}".format(account.name, v.name): v for v in account.account.vehicles})
+            self.targets.update({v.name: v for v in account.account.vehicles})
 
-    def send_message(self, message=None, **kwargs):
-        """Send the message to the car."""
+    def send_message(self, message="", **kwargs):
+        """Send a message or POI to the car."""
 
         for _vehicle in kwargs[ATTR_TARGET]:
-            _LOGGER.error("Sending message to %s", _vehicle.name) # TODO
-            #TODO the above logger is set to error to see the message in the log easily -> change to debug
+            _LOGGER.debug("Sending message to %s", _vehicle.name)
 
             # Extract params from data dict
             title = kwargs.get(ATTR_TITLE, ATTR_TITLE_DEFAULT)
@@ -58,20 +55,21 @@ class BMWNotificationService(BaseNotificationService):
             # Check if message is a POI
             if data is not None and ATTR_LOCATION in data:
                 location_dict = {
-                        ATTR_LAT: data[ATTR_LOCATION][ATTR_LATITUDE],
-                        ATTR_LON: data[ATTR_LOCATION][ATTR_LONGITUDE],
-                        ATTR_NAME: message,
-                    }
+                    ATTR_LAT: data[ATTR_LOCATION][ATTR_LATITUDE],
+                    ATTR_LON: data[ATTR_LOCATION][ATTR_LONGITUDE],
+                    ATTR_NAME: message,
+                }
                 # Update dictionary with additional attributes if available
-                location_dict.update({k: v for k, v in data[ATTR_LOCATION].items() if k in ATTR_LOCATION_ATTRIBUTES})
-
-                _vehicle.remote_services.trigger_send_poi(
-                    location_dict
+                location_dict.update(
+                    {
+                        k: v
+                        for k, v in data[ATTR_LOCATION].items()
+                        if k in ATTR_LOCATION_ATTRIBUTES
+                    }
                 )
+
+                _vehicle.remote_services.trigger_send_poi(location_dict)
             else:
                 _vehicle.remote_services.trigger_send_message(
-                    {
-                        ATTR_TEXT: message,
-                        ATTR_SUBJECT: title,
-                    }
+                    {ATTR_TEXT: message, ATTR_SUBJECT: title,}
                 )
