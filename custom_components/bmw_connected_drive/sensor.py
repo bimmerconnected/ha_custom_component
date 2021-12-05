7ef5a81963535a535e8ebe2a7f3f37297931f95b
rikroe <rikroe@users.noreply.github.com>
2021-12-05 15:01:12 +0100

    Update from core, bump bimmer_connected to 0.8.5

--- custom_components/bmw_connected_drive/sensor.py
+++ custom_components/bmw_connected_drive/sensor.py
@@ -1,10 +1,10 @@
 """Support for reading vehicle status from BMW connected drive portal."""
 from __future__ import annotations
 
-from collections.abc import Callable, Mapping
+from collections.abc import Callable
 from dataclasses import dataclass
 import logging
-from typing import Any, cast
+from typing import cast
 
 from bimmer_connected.vehicle import ConnectedDriveVehicle
 
@@ -43,7 +43,6 @@ class BMWSensorEntityDescription(SensorEntityDescription):
     unit_metric: str | None = None
     unit_imperial: str | None = None
     value: Callable = lambda x, y: x
-    extra_attributes: dict | None = None
 
 
 SENSOR_TYPES: dict[str, BMWSensorEntityDescription] = {
@@ -56,9 +55,11 @@ SENSOR_TYPES: dict[str, BMWSensorEntityDescription] = {
     ),
     "charging_end_time": BMWSensorEntityDescription(
         key="charging_end_time",
-        icon="mdi:update",
         device_class=DEVICE_CLASS_TIMESTAMP,
-        extra_attributes={"original_value": "charging_end_time_original"},
+    ),
+    "charging_end_time_mybmw": BMWSensorEntityDescription(
+        key="charging_end_time_original",
+        entity_registry_enabled_default=False,
     ),
     "charging_status": BMWSensorEntityDescription(
         key="charging_status",
@@ -179,13 +180,3 @@ class BMWConnectedDriveSensor(BMWConnectedDriveBaseEntity, SensorEntity):
         """Return the state."""
         state = getattr(self._vehicle.status, self.entity_description.key)
         return cast(StateType, self.entity_description.value(state, self.hass))
-
-    @property
-    def extra_state_attributes(self) -> Mapping[str, Any] | None:
-        """Return the attributes."""
-        if self.entity_description.extra_attributes:
-            return {
-                k: getattr(self._vehicle.status, v)
-                for k, v in self.entity_description.extra_attributes.items()
-            }
-        return None
