2cb025b05c6e56ab68c489285e75d5e66cb418e8
rikroe <rikroe@users.noreply.github.com>
2021-12-21 20:15:23 +0000

    Update to Home Assistant 2021.12.4

--- custom_components/bmw_connected_drive/__init__.py
+++ custom_components/bmw_connected_drive/__init__.py
@@ -18,6 +18,7 @@ from homeassistant.const import (
     CONF_PASSWORD,
     CONF_REGION,
     CONF_USERNAME,
+    Platform,
 )
 from homeassistant.core import HomeAssistant, ServiceCall, callback
 from homeassistant.exceptions import ConfigEntryNotReady
@@ -65,7 +66,13 @@ DEFAULT_OPTIONS = {
     CONF_READ_ONLY: False,
 }
 
-PLATFORMS = ["binary_sensor", "device_tracker", "lock", "notify", "sensor"]
+PLATFORMS = [
+    Platform.BINARY_SENSOR,
+    Platform.DEVICE_TRACKER,
+    Platform.LOCK,
+    Platform.NOTIFY,
+    Platform.SENSOR,
+]
 UPDATE_INTERVAL = 5  # in minutes
 
 SERVICE_UPDATE_STATE = "update_state"
@@ -148,7 +155,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     await _async_update_all()
 
     hass.config_entries.async_setup_platforms(
-        entry, [platform for platform in PLATFORMS if platform != NOTIFY_DOMAIN]
+        entry, [platform for platform in PLATFORMS if platform != Platform.NOTIFY]
     )
 
     # set up notify platform, no entry support for notify platform yet,
@@ -169,7 +176,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
     unload_ok = await hass.config_entries.async_unload_platforms(
-        entry, [platform for platform in PLATFORMS if platform != NOTIFY_DOMAIN]
+        entry, [platform for platform in PLATFORMS if platform != Platform.NOTIFY]
     )
 
     # Only remove services if it is the last account and not read only
