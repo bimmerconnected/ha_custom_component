a839861f86ceeea42b3cff0bd4c612e94f9a21d9
rikroe <42204099+rikroe@users.noreply.github.com>
2022-01-17 17:48:09 +0100

    Update sensor.py

--- custom_components/bmw_connected_drive/sensor.py
+++ custom_components/bmw_connected_drive/sensor.py
@@ -46,11 +46,18 @@ class BMWSensorEntityDescription(SensorEntityDescription):
 
 SENSOR_TYPES: dict[str, BMWSensorEntityDescription] = {
     # --- Generic ---
-    "charging_time_remaining": BMWSensorEntityDescription(
-        key="charging_time_remaining",
-        icon="mdi:update",
-        unit_metric=TIME_HOURS,
-        unit_imperial=TIME_HOURS,
+    "charging_start_time": BMWSensorEntityDescription(
+        key="charging_start_time",
+        device_class=DEVICE_CLASS_TIMESTAMP,
+        entity_registry_enabled_default=False,
+    ),
+    "charging_end_time": BMWSensorEntityDescription(
+        key="charging_end_time",
+        device_class=DEVICE_CLASS_TIMESTAMP,
+    ),
+    "charging_time_label": BMWSensorEntityDescription(
+        key="charging_time_label",
+        entity_registry_enabled_default=False,
     ),
     "charging_status": BMWSensorEntityDescription(
         key="charging_status",
