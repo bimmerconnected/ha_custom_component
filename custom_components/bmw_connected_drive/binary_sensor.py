6136c9939fc57d64a3dd50e8673cae5380ceaa3b
rikroe <rikroe@users.noreply.github.com>
2023-05-03 22:41:07 +0200

    Update to Home Assistant 2023.5.0

--- custom_components/bmw_connected_drive/binary_sensor.py
+++ custom_components/bmw_connected_drive/binary_sensor.py
@@ -189,12 +189,6 @@ SENSOR_TYPES: tuple[BMWBinarySensorEntityDescription, ...] = (
         icon="mdi:car-electric",
         value_fn=lambda v: v.fuel_and_battery.is_charger_connected,
     ),
-    BMWBinarySensorEntityDescription(
-        key="is_pre_entry_climatization_enabled",
-        name="Pre entry climatization",
-        icon="mdi:car-seat-heater",
-        value_fn=lambda v: v.charging_profile.is_pre_entry_climatization_enabled,
-    ),
 )
 
 
